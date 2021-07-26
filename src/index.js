b3362a0f18b41e3d4b5809a7a1c5c639d713991f
murod91 <dulce.muro91@gmail.com>
2021-07-25 18:45:32 -0700

    JS weather structure

---
+++ src/index.js
@@ -0,0 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let precipitationElement = document.querySelector("#precipitation");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  precipitationElement.innerHTML = response.data.main.precipitation;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = `d36264de255bf95da0a082dd213ebaf4`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
