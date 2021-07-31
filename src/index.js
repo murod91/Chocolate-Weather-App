40d8fb869281abfc8cc411c649b2085af32a5595
murod91 <dulce.muro91@gmail.com>
2021-07-30 22:45:37 -0700

    Added Weather Forecast to JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thur", "Fri", "Sat", "Sun", "Mon", "Tues", "Wed"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+          <div class="weather-forecast-date">${day}</div>
+            <img
+             src="https://openweathermap.org/img/wn/50d@2x.png"
+              alt=""
+              width="42"
+              />
+              <br />
+              <div class="weather-forecast-temp">
+                <span class="weather-forecast-temp-max">18°</span>
+                <span class="weather-forecast-temp-min">12°</span>
+              </div>
+      </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -32,6 +58,7 @@ function handleSubmit(event) {
 }
 
 search("New York");
+displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
